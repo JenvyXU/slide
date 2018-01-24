49c16298588790a350ac81f2480f8bf751624cee
xzw <1272117264@qq.com>
2018-01-24 23:16:04 +0800

    优化代码

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 
 let n
 初始化()
-
 setInterval(()=>{
-     $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend',(e)=>{
-        $(e.currentTarget).removeClass('leave').addClass('enter')
+    makeLeave(getImage(n)).one('transitionend',(e)=>{
+        makeEnter($(e.currentTarget))
     })
-    console.log(n)
-    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+/*     $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one('transitionend',(e)=>{
+        $(e.currentTarget).removeClass('leave').addClass('enter')
+    })   */
+    makeCurrent(getImage(n+1))
+/*    $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current') */
     n+=1  
-    console.log(n)
 },3000)
-//  1 2 2 3 3 1 1 2 3 1 1
+
+
+
+
 function x(n){
     if(n>3){
         n=n%3
@@ -20,6 +24,9 @@ function x(n){
     return n
 }
 
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
  function 初始化(){
     n=1
     $(`.images>img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
@@ -28,6 +35,16 @@ function x(n){
     $('.images>img:nth-child(2)').addClass('enter')
     $('.images>img:nth-child(3)').addClass('enter')
     */   
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
 }
 
 
